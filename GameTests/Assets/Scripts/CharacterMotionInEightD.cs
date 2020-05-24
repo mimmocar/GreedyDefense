80fc9d7a8f39ab5ed41e4bd3c2f96fd4eb04baaa
mimmocar <mimmo.carapella@gmail.com>
2020-05-24 18:21:21 +0200

    test

---
+++ GameTests/Assets/Scripts/CharacterMotionInEightD.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotionInEightD : MonoBehaviour
+{
+    protected CharacterController _charController;
+    public float velocity = 5;
+    public float runBoost = 5;
+    public float turnSpeed = 1;
+    protected float rotationSensitivity = 3.0f;
+    Vector2 input;
+    float angle;
+    Vector3 movement;
+    Animator anim;
+    Quaternion targetRotation;
+    Transform cam;
+    Camera followingCamera;
+
+    protected CharacterStatus status;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        followingCamera = Camera.main;
+        cam = GameObject.Find("FollowingCamera").transform;
+        status = GetComponent<CharacterStatus>();
+        _charController = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (followingCamera.enabled)
+        {
+            anim.enabled = true;
+            if (!status.IsMoving && !status.IsRotating)
+            {
+                anim.SetBool("isMoving", false);
+                anim.SetBool("rotation", false);
+                return;
+
+            }
+            GetInput();
+            CalculateDirection();
+            Rotate();
+            Move();
+        }
+        else
+        {
+            anim.enabled = false;
+            AlternativeMovement();
+        }
+    }
+
+    void AlternativeMovement()
+    {
+        Vector3 movement = new Vector3(0, 0, 0);
+        if (status.IsMoving)
+        {
+
+            float vertMovement = status.Movement * velocity;
+            if (status.IsRunning)
+            {
+                vertMovement *= runBoost;
+            }
+            movement.z = vertMovement * Time.deltaTime;
+
+        }
+
+        movement = transform.TransformDirection(movement);
+
+        _charController.Move(movement);
+
+        if (status.IsRotating)
+        {
+
+            transform.Rotate(0, status.Rotation * rotationSensitivity, 0);
+        }
+
+    }
+
+
+
+    void GetInput()
+    {
+        input.x = status.Rotation;
+        input.y = status.Movement;
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+        Debug.Log(angle);
+        if (angle < 0) anim.SetBool("rotation", true);
+        else anim.SetBool("rotation", false);
+
+    }
+
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+    private void Move()
+    {
+        //transform.position += transform.forward * velocity * Time.deltaTime;
+        movement = transform.forward * velocity * Time.deltaTime;
+        _charController.Move(movement);
+        anim.SetBool("isMoving", true);
+
+    }
+}
---
+++ GameTests/Assets/Scripts/FollowingChar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingChar : MonoBehaviour
+{
+    [SerializeField] Transform PlayerTransform;
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    [SerializeField] float SmoothFactor = 0.5f;
+
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    void LateUpdate()
+    {
+
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+}
