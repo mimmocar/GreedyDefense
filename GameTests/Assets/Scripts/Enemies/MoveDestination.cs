7130bc6148b11f595f743817c2620d13c8f512c9
mimmocar <mimmo.carapella@gmail.com>
2020-06-21 17:39:53 +0200

    Update Environment

---
+++ GameTests/Assets/Scripts/Enemies/MoveDestination.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveDestination : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform goal;
+    void Start()
+    {
+        goal = GameObject.FindWithTag("food").transform;
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+}
---
+++ GameTests/Assets/Scripts/Enemies/WaveSpawner.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+
+    public float timeBetweenWaves = 5f;
+    private float waveCountDown;
+
+    private float searchCountDown = 1f;
+
+    public SpawnState state = SpawnState.COUNTING;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveCountDown = timeBetweenWaves;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive())
+            {
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (waveCountDown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountDown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave completed");
+
+        state = SpawnState.COUNTING;
+        waveCountDown = timeBetweenWaves;
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("All waves complete!");
+        }
+        else
+        {
+            nextWave++;
+        }
+
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountDown -= Time.deltaTime;
+        if (searchCountDown <= 0f)
+        {
+            searchCountDown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemey") == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning wave: " + _wave.name);
+        state = SpawnState.SPAWNING;
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Instantiate(_enemy, transform.position, transform.rotation);
+        Debug.Log("Spawning Enemy: " + _enemy.name);
+    }
+}
