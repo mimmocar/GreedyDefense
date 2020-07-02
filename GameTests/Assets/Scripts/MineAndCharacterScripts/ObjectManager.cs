a27726077cce9a4ca959516864f796a1bf271dd9
mimmocar <mimmo.carapella@gmail.com>
2020-07-02 09:27:15 +0200

    Added kills updating

--- GameTests/Assets/Scripts/MineAndCharacterScripts/ObjectManager.cs
+++ GameTests/Assets/Scripts/MineAndCharacterScripts/ObjectManager.cs
@@ -6,20 +6,20 @@ using UnityEngine;
 public class ObjectManager : MonoBehaviour
 {
     //private delegate void OnSpawnObject(Vector3 pos, GameObject pref);
-    // Start is called before the first frame update
-    private int count = 0;
+    //Start is called before the first frame update
+    private int hit = 0, kills = 0, berserk = 100;
     [SerializeField] GameObject[] prefab;
     void Awake()
     {
         Messenger<Vector3, int>.AddListener(GameEvent.SPAWN_REQUESTED, OnSpawnObject);
-        Messenger<GameObject,int>.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+        Messenger<GameObject, int>.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
 
     }
 
     void OnDestroy()
     {
         Messenger<Vector3, int>.RemoveListener(GameEvent.SPAWN_REQUESTED, OnSpawnObject);
-        Messenger<GameObject,int>.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+        Messenger<GameObject, int>.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
     }
 
 
@@ -36,16 +36,27 @@ public class ObjectManager : MonoBehaviour
         Instantiate(prefab[i], position, new Quaternion(0, 0, 0, 0));
     }
 
-    void OnEnemyHit(GameObject enemy, int damage)
+    void OnEnemyHit(GameObject target, int damage)
     {
-        count += 1;
-        Debug.Log("Invocazione n " + count);
+        Enemy enemy = target.GetComponent<Enemy>();
+        hit += 1;
+        Debug.Log("Invocazione n " + hit);
         //enemy.GetComponent<EnemyStatus>().Stamina -= 1;
-        enemy.GetComponent<Enemy>().Health -= damage; 
+        //enemy.GetComponent<Enemy>().Health -= damage;
+        enemy.TakeDamage(damage);
         Debug.Log(enemy + "  Stamina is: " + enemy.GetComponent<Enemy>().Health);
-        if (enemy.GetComponent<Enemy>().Health == 0)
+        if (/*enemy.GetComponent<Enemy>().Health == 0*/enemy.Dead)
         {
-            Destroy(enemy);
+            //Destroy(enemy.gameObject);
+            enemy.Die();
+            
+            if(kills >= berserk)
+            {
+                kills = 0;
+            }
+
+            kills++;
+            Messenger<int, int>.Broadcast(GameEvent.ENEMY_DIED, kills, berserk);
         }
 
     }
