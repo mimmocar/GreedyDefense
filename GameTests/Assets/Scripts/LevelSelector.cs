ce8718afbc9adf1d7a0125aed64bf15f23ea52b1
andreadv1997 <andreadv1997@gmail.com>
2020-08-13 19:34:09 +0200

    Added Level Selector: still to connect to other scenes

---
+++ GameTests/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSelector : MonoBehaviour
+{
+    
+    public UnityEngine.UI.Button[] levelButtons;
+
+    private void Start()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if(i+1 > levelReached)
+                levelButtons[i].interactable = false;
+
+            Debug.Log("Elemento trovato +", GameObject.Find("Border1"));
+        }
+    }
+
+    public void Select(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
