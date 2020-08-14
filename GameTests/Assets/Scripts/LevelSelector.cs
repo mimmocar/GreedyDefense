533363b49539c47c0994e6c12620e1f5cee14b2f
andreadv1997 <andreadv1997@gmail.com>
2020-08-14 11:47:21 +0200

    Added score stars on GUI

--- GameTests/Assets/Scripts/LevelSelector.cs
+++ GameTests/Assets/Scripts/LevelSelector.cs
@@ -13,10 +13,29 @@ public class LevelSelector : MonoBehaviour
         int levelReached = PlayerPrefs.GetInt("levelReached", 1);
         for (int i = 0; i < levelButtons.Length; i++)
         {
-            if(i+1 > levelReached)
+            if (i + 1 > levelReached)
                 levelButtons[i].interactable = false;
+            else
+            {
+                int score = PlayerPrefs.GetInt("starForLevel" + i + 1, 0);
 
-            Debug.Log("Elemento trovato +", GameObject.Find("Border1"));
+                //Image[] borders = levelButtons[i].GetComponentsInChildren<Image>();
+                //Debug.Log("Number of borders found : "+ borders.Length);
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j + 1 <= score)
+                    {
+                        Transform border = levelButtons[i].transform.GetChild(j + 1);
+                        if (border != null)
+                            Debug.Log("Border found : " + border);
+                        Transform starImage = border.GetChild(0);
+                        Debug.Log("Border found : " + starImage);
+                        starImage.gameObject.SetActive(true);
+                    }
+
+                    else break;
+                }
+            }
         }
     }
 
